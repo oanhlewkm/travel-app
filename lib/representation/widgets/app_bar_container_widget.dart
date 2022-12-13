6f1fa9ac40505ccbc07882451371bb9d98da4e97
oanhlewkm <oanhle@wakumo.vn>
2022-12-13 16:52:31 +0700

    add custome navbar

--- lib/representation/widgets/app_bar_container_widget.dart
+++ lib/representation/widgets/app_bar_container_widget.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:new_app/core/constants/color_palatte.dart';
 import 'package:new_app/core/constants/dimension_constants.dart';
 import 'package:new_app/core/helpers/asset_helper.dart';
 import 'package:new_app/core/helpers/image_helper.dart';
 
 class AppBarContainerWidge extends StatelessWidget {
-  const AppBarContainerWidge({super.key, required this.child});
+  const AppBarContainerWidge({
+    super.key,
+    required this.child,
+    this.title,
+    this.titleString,
+    this.implementLeading = false,
+  });
 
   final Widget child;
+  final Widget? title;
+  final String? titleString;
+  final bool implementLeading;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +31,39 @@ class AppBarContainerWidge extends StatelessWidget {
               automaticallyImplyLeading: false,
               elevation: 0,
               toolbarHeight: 90,
+              title: title ??
+                  Row(
+                    children: [
+                      if (implementLeading)
+                        Container(
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(kDefaultPadding),
+                            ),
+                          ),
+                          color: Colors.white,
+                          child: const Icon(
+                            FontAwesomeIcons.backward,
+                            color: Colors.black,
+                            size: kDefaultIconSize,
+                          ),
+                        ),
+                      Expanded(
+                          child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              titleString ?? '',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
               backgroundColor: ColorPalette.backgroundScaffoldColor,
               flexibleSpace: Stack(
                 children: [
